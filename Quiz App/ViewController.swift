644e0981a36d174f4051ee408974b7eac848ba59
John Keatings <keatingsjohn1@gmail.com>
2022-01-12 19:39:39 -0500

    Added timer functionality

--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -11,9 +11,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
     
     var quiz = [
-        Question(text: "There are 7 planets in our solar system", answer: "True"),
+        Question(text: "There are 7 planets in our solar system", answer: "False"),
         Question(text: "Each planet in our solar system contains oxygen", answer: "False"),
         Question(text: "Carbon is the most frequent element on earth", answer: "False"),
         Question(text: "Sunlight requires nearly 25 seconds to reach Earth", answer: "False"),
@@ -25,33 +26,73 @@ class ViewController: UIViewController {
     ]
 
     var questionNumber = 0
+    var timeRemaining = 15
+    var timer = Timer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         questionLabel.text = quiz[questionNumber].text
         answerLabel.text = ""
+        timerLabel.text = "\(timeRemaining)"
     }
 
 
     @IBAction func actionKeyPressed(_ sender: UIButton) {
         
         if sender.titleLabel?.text! == quiz[questionNumber].answer {
-            answerLabel.text = "Right"
+            answerLabel.text = "Correct"
+            resetTimer()
         } else {
-            answerLabel.text = "Wrong"
+            answerLabel.text = "Incorrect"
+            resetTimer()
         }
-        
         updateUI()
     }
     
     func updateUI() {
+        updateQuestion()
+        updateTimer()
+    }
+    
+    func runTimer() {
+     timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+
+        if timeRemaining >= 0 {
+            timerLabel.text = "\(timeRemaining)"
+            timeRemaining -= 1
+        } else {
+            noKeyPressed()
+        }
+        
+    }
+    
+    func resetTimer() {
+        timer.invalidate()
+        timeRemaining = 15
+        timerLabel.text = "\(timeRemaining)"
+        runTimer()
+    }
+    
+    func updateQuestion() {
         if questionNumber < quiz.count - 1 {
             questionNumber += 1
         } else {
+            // game over screen and score should appear here
             questionNumber = 0
         }
         
         questionLabel.text = quiz[questionNumber].text
     }
+    
+    func noKeyPressed() {
+        answerLabel.text = "Passed"
+        resetTimer()
+        updateUI()
+    }
+    
 }
