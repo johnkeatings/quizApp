2887dde647e72bb63c918df56f7a449125c03ff6
John Keatings <keatingsjohn1@gmail.com>
2022-01-14 18:56:16 -0500

    refactored

---
+++ Quiz App/Controller/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Quiz App
+//
+//  Created by John Keatings on 2022-01-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        questionLabel.text = quizBrain.quiz[quizBrain.questionNumber].text
+        answerLabel.text = "Select answer to start quiz"
+    }
+
+
+    @IBAction func actionKeyPressed(_ sender: UIButton) {
+        let userAnswer = sender.titleLabel?.text!
+        
+        if userAnswer == quizBrain.quiz[quizBrain.questionNumber].answer {
+            answerLabel.text = "Correct"
+        } else {
+            answerLabel.text = "Incorrect"
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        quizBrain.updateQuestion()
+        questionLabel.text = quizBrain.quiz[quizBrain.questionNumber].text
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
+            self.answerLabel.text = ""
+        }
+    }
+
+}
